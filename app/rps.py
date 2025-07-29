80312812fff511366ac7d9c00b9cf7f91070aa30
Errol E. <elbasanerrol@gmail.com>
2025-07-29 19:18:29 -0400

    Refactored winner determination logic

--- app/rps.py
+++ app/rps.py
@@ -2,8 +2,25 @@
 import random
 VALID_OPTIONS = ['rock','paper','scissor']
 
-print("Welcome to the game!")
+def determine_winner(player_choice, computer_choice):
+    if player_choice == computer_choice:
+        result = "TIE GAME"
+    elif (player_choice == 'rock' and computer_choice == 'scissor'):
+        result = "USER WIN"
+    elif (player_choice == 'rock' and computer_choice == 'paper'):
+        result = "COMP WIN"
+    elif (player_choice == 'scissor' and computer_choice == 'rock'):
+        result = "COMP WIN"
+    elif (player_choice == 'scissor' and computer_choice == 'paper'):
+        result = "USER WIN"
+    elif (player_choice == 'paper' and computer_choice == 'scissor'):
+        result = "COMP WIN"
+    elif (player_choice == 'paper' and computer_choice == 'rock'):
+        result = "USER WIN"
+    return (result)
 
+
+print("Welcome to the game!")
 
 player_choice = input(f"Select an option {VALID_OPTIONS}:")
 print("user choice:", player_choice)
@@ -11,19 +28,4 @@ print("user choice:", player_choice)
 computer_choice = random.choice(VALID_OPTIONS)
 print("COMPUTER CHOOSES:", computer_choice)
 
-if player_choice == computer_choice:
-    result = "TIE GAME"
-elif (player_choice == 'rock' and computer_choice == 'scissor'):
-    result = "USER WIN"
-elif (player_choice == 'rock' and computer_choice == 'paper'):
-    result = "COMP WIN"
-elif (player_choice == 'scissor' and computer_choice == 'rock'):
-    result = "COMP WIN"
-elif (player_choice == 'scissor' and computer_choice == 'paper'):
-    result = "USER WIN"
-elif (player_choice == 'paper' and computer_choice == 'scissor'):
-    result = "COMP WIN"
-elif (player_choice == 'paper' and computer_choice == 'rock'):
-    result = "USER WIN"
-    
-print(result)
+print(determine_winner(player_choice, computer_choice))
