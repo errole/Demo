95547c739b2bc886652e29476faaed0652c2212b
Errol E. <elbasanerrol@gmail.com>
2025-07-29 20:23:16 -0400

    Refactored code to be able to run tests

--- app/rps.py
+++ app/rps.py
@@ -1,4 +1,5 @@
 # Rock Paper Scissors Game
+
 import random
 VALID_OPTIONS = ['rock','paper','scissor']
 
@@ -19,13 +20,17 @@ def determine_winner(player_choice, computer_choice):
         result = "USER WIN"
     return (result)
 
+# only run code indented inside 
+# if we are running this script from the command line
+# but not if we are importing
+if __name__ == "__main__":
+    print("Welcome to the game!")
 
-print("Welcome to the game!")
+    player_choice = input(f"Select an option {VALID_OPTIONS}:")
+    print("user choice:", player_choice)
 
-player_choice = input(f"Select an option {VALID_OPTIONS}:")
-print("user choice:", player_choice)
+    computer_choice = random.choice(VALID_OPTIONS)
+    print("COMPUTER CHOOSES:", computer_choice)
 
-computer_choice = random.choice(VALID_OPTIONS)
-print("COMPUTER CHOOSES:", computer_choice)
+    print(determine_winner(player_choice, computer_choice))
 
-print(determine_winner(player_choice, computer_choice))
